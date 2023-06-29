8f13eb803c8b9552f7ac62fedcded4d6f6555b5b
Andrey Zhmako <andreyzhmako47@gmail.com>
2023-06-29 18:32:08 +0300

    Added components Footer and Header

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,14 @@ import { // единое место импорта всех страниц из 
   Profile
 } from "./pages";
 import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 
 function App() {
   return <>
-    <Layout>
-      <ul className="menu">
-        <li><Link to="/">Главная</Link></li>
-        <li><Link to="/catalog">Каталог</Link>
-          <ul>
-            <li><Link to="/catalog/Snacks">Лакомства</Link></li>
-            <li><Link to="/catalog/Toys">Игрушки</Link></li>
-          </ul>
-        </li>
-        <li><Link to="/favorites">Избранное</Link></li>
-        <li><Link to="/product/Ball">Мячик для собак</Link></li>
-        <li><Link to="/addProduct">Добавить товар</Link></li>
-        <li><Link to="/cart">Корзина</Link></li>
-        <li><Link to="/profile">Профиль</Link></li>
-        <li><Link to="/auth">Войти</Link></li>
-        <li><Link to="/content/Delivery">Доставка</Link></li>
-        <li><Link to="/content/About">О нас</Link></li>
-
-      </ul>
-    </Layout>
     
+    <Header />
     <Routes> {/* Пути ко всем страницам сайта */}
       <Route path="/" element={<Home/>}/>  {/* простой слеш это главная страница */}
       <Route path="/catalog" element={<Catalog/>}/>  {/* каталог товаров */}
@@ -52,6 +35,8 @@ function App() {
 
       <Route path="/content/about" element={<ContentAbout/>}/>  {/* статика: информация о магазине */}
     </Routes>
+
+    <Footer />
   </>
 }
 
